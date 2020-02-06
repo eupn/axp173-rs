48bf61d4414294b91aa2cab51d607f0d0c322979
Evgenii P <eupn@protonmail.com>
2020-02-07 00:32:58 +0800

    Refactor Result types

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,9 @@ const AXP173_ADDR: u8 = 0x68;
 /// Datasheet, p. 28, section 9.11.
 const AXP173_ON_CHIP_BUFFER_DEFAULT: [u8; 6] = [0xf0, 0x0f, 0x00, 0xff, 0x00, 0x00];
 
+type Axp173Result<T, E> = Result<T, Error<E>>;
+type OperationResult<E> = Axp173Result<(), E>;
+
 /// All possible errors in this crate
 #[derive(Debug)]
 pub enum Error<E> {
@@ -56,7 +59,7 @@ where
     /// Checks the I2C connection to the AXP173 chip.
     /// AXP173 doesn't have a dedicated chip ID register and connection is checked by reading
     /// on-chip buffer for a presence of default values.
-    pub fn init(&mut self) -> Result<(), Error<E>> {
+    pub fn init(&mut self) -> OperationResult<E> {
         self.read_u8(POWER_DATA_BUFFER1).map_err(Error::I2c)?;
 
         let buf = self.read_onchip_buffer()?;
@@ -68,7 +71,7 @@ where
     }
 
     /// Reads 6-byte user data buffer from the chip.
-    pub fn read_onchip_buffer(&mut self) -> Result<[u8; 6], Error<E>> {
+    pub fn read_onchip_buffer(&mut self) -> Axp173Result<[u8; 6], E> {
         let mut buf = [0u8; 6];
         self.read_bytes(POWER_DATA_BUFFER1, &mut buf)
             .map_err(Error::I2c)?;
@@ -78,7 +81,7 @@ where
 
     /// Writes 6-byte user data buffer into the chip.
     #[allow(clippy::trivially_copy_pass_by_ref)] // On 32-bit ARMs it is not efficient to pass by value
-    pub fn write_onchip_buffer(&mut self, bytes: &[u8; 6]) -> Result<(), E> {
+    pub fn write_onchip_buffer(&mut self, bytes: &[u8; 6]) -> OperationResult<E> {
         let mut buf = [0u8; 6 + 1];
 
         buf[0] = POWER_DATA_BUFFER1;
@@ -86,44 +89,44 @@ where
             buf[i + 1] = *byte;
         }
 
-        self.i2c.write(AXP173_ADDR, &buf[..])?;
+        self.i2c.write(AXP173_ADDR, &buf[..]).map_err(Error::I2c)?;
 
         Ok(())
     }
 
     /// Returns `true` if device is connected to the USB power source.
-    pub fn vbus_present(&mut self) -> Result<bool, Error<E>> {
+    pub fn vbus_present(&mut self) -> Axp173Result<bool, E> {
         let reg_val = self.read_u8(POWER_STATUS).map_err(Error::I2c)?;
 
         Ok(reg_val.get_bit(POWER_STATUS_VBUS_PRESENT))
     }
 
     /// Returns `true` if lithium battery is connected.
-    pub fn battery_present(&mut self) -> Result<bool, Error<E>> {
+    pub fn battery_present(&mut self) -> Axp173Result<bool, E> {
         let reg_val = self.read_u8(POWER_MODE_CHGSTATUS).map_err(Error::I2c)?;
 
         Ok(reg_val.get_bit(POWER_MODE_CHGSTATUS_BATTERY_PRESENT))
     }
 
     /// Returns `true` if lithium battery is connected and currently charging.
-    pub fn battery_charging(&mut self) -> Result<bool, Error<E>> {
+    pub fn battery_charging(&mut self) -> Axp173Result<bool, E> {
         let reg_val = self.read_u8(POWER_MODE_CHGSTATUS).map_err(Error::I2c)?;
 
         Ok(reg_val.get_bit(POWER_MODE_CHGSTATUS_IS_CHARGING))
     }
 
     /// Enables selected LDO with selected output voltage.
-    pub fn enable_ldo(&mut self, ldo: &Ldo) -> Result<(), Error<E>> {
+    pub fn enable_ldo(&mut self, ldo: &Ldo) -> OperationResult<E> {
         self.set_ldo_voltage(&ldo)?;
         self.switch_ldo(&ldo.kind, true)
     }
 
     /// Disables selected LDO.
-    pub fn disable_ldo(&mut self, ldo: &LdoKind) -> Result<(), Error<E>> {
+    pub fn disable_ldo(&mut self, ldo: &LdoKind) -> OperationResult<E> {
         self.switch_ldo(ldo, false)
     }
 
-    fn switch_ldo(&mut self, ldo: &LdoKind, enable: bool) -> Result<(), Error<E>> {
+    fn switch_ldo(&mut self, ldo: &LdoKind, enable: bool) -> OperationResult<E> {
         let bit = match ldo {
             LdoKind::LDO2 => POWER_ON_OFF_REG_LDO2_ON,
             LdoKind::LDO3 => POWER_ON_OFF_REG_LDO3_ON,
@@ -139,7 +142,7 @@ where
         Ok(())
     }
 
-    fn set_ldo_voltage(&mut self, ldo: &Ldo) -> Result<(), Error<E>> {
+    fn set_ldo_voltage(&mut self, ldo: &Ldo) -> OperationResult<E> {
         let reg = match &ldo.kind {
             LdoKind::LDO2 | LdoKind::LDO3 => LDO2_LDO3_OUT_VOL_REG,
             LdoKind::LDO4 => LDO4_OUT_VOL_REG,
@@ -164,7 +167,7 @@ where
 
     /// Sets charging current of the battery. Adjust this for an efficient and safe charging of
     /// your lithium battery's capacity.
-    pub fn set_charging_current(&mut self, current: ChargingCurrent) -> Result<(), Error<E>> {
+    pub fn set_charging_current(&mut self, current: ChargingCurrent) -> OperationResult<E> {
         let mut bits = self.read_u8(POWER_CHARGE1).map_err(Error::I2c)?;
         bits.set_bits(POWER_CHARGE1_CURRENT_SETTING, current.bits());
 
@@ -176,7 +179,7 @@ where
     /// Sets battery charging regulation voltage. This value varies depending on battery's chemistry
     /// and should be as close to the value from battery's datasheet as possible to ensure
     /// efficient charging and long battery life.
-    pub fn set_charging_voltage(&mut self, voltage: ChargingVoltage) -> Result<(), Error<E>> {
+    pub fn set_charging_voltage(&mut self, voltage: ChargingVoltage) -> OperationResult<E> {
         let mut bits = self.read_u8(POWER_CHARGE1).map_err(Error::I2c)?;
         bits.set_bits(POWER_CHARGE1_VOLTAGE_SETTING, voltage.bits());
 
@@ -186,7 +189,7 @@ where
     }
 
     /// Enables or disables battery charging.
-    pub fn set_charging(&mut self, enabled: bool) -> Result<(), Error<E>> {
+    pub fn set_charging(&mut self, enabled: bool) -> OperationResult<E> {
         let mut bits = self.read_u8(POWER_CHARGE1).map_err(Error::I2c)?;
         bits.set_bit(POWER_CHARGE1_ENABLE_CHARGING, enabled);
 
@@ -196,7 +199,7 @@ where
     }
 
     /// Enables or disables certain ADC functions of AXP173.
-    pub fn set_adc_settings(&mut self, adc_settings: &AdcSettings) -> Result<(), Error<E>> {
+    pub fn set_adc_settings(&mut self, adc_settings: &AdcSettings) -> OperationResult<E> {
         // Set the contents of ADC enable/disable register.
         let mut bits = self.read_u8(POWER_ADC_EN1).map_err(Error::I2c)?;
         adc_settings.write_adc_en_bits(&mut bits);
