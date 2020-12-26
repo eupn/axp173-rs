d9b417c23e84b6dcb10a8fa311677cfa417a645b
Evgenii P <eupn@protonmail.com>
2020-12-26 21:36:47 +0700

    ldo.rs: read-out the voltage setting

--- src/ldo.rs
+++ src/ldo.rs
@@ -1,4 +1,9 @@
+/// Newtype wrapper for MilliVolts.
+#[derive(Debug, Copy, Clone)]
+pub struct MilliVolts(pub usize);
+
 /// Voltage regulators in AXP173 that can be enabled or disabled.
+#[derive(Debug)]
 pub enum LdoKind {
     /// LDO2.
     LDO2,
@@ -11,9 +16,11 @@ pub enum LdoKind {
 }
 
 /// LDO of AXP173.
+#[derive(Debug)]
 pub struct Ldo {
     pub(crate) kind: LdoKind,
     pub(crate) voltage: u8,
+    pub(crate) enabled: bool,
 }
 
 impl Ldo {
@@ -21,12 +28,13 @@ impl Ldo {
     /// `voltage`: four bit voltage: 1.8 - 3.3V, 100 mV per LSB.
     /// # Panics
     /// Panics if supplied voltage value is greater than 2^4 due to failed assertion.
-    pub fn ldo2_with_voltage(voltage: u8) -> Self {
+    pub fn ldo2_with_voltage(voltage: u8, enabled: bool) -> Self {
         assert!(voltage <= 0b1111); // Only 4-bit wide
 
         Self {
             kind: LdoKind::LDO2,
             voltage,
+            enabled,
         }
     }
 
@@ -35,12 +43,13 @@ impl Ldo {
     ///
     /// # Panics
     /// Panics if supplied voltage value is greater than 2^4 due to failed assertion.
-    pub fn ldo3_with_voltage(voltage: u8) -> Self {
+    pub fn ldo3_with_voltage(voltage: u8, enabled: bool) -> Self {
         assert!(voltage <= 0b1111); // Only 4-bit wide
 
         Self {
             kind: LdoKind::LDO3,
             voltage,
+            enabled,
         }
     }
 
@@ -49,12 +58,44 @@ impl Ldo {
     ///
     /// # Panics
     /// Panics if supplied voltage value is greater than 2^7 due to failed assertion.
-    pub fn ldo4_with_voltage(voltage: u8) -> Self {
+    pub fn ldo4_with_voltage(voltage: u8, enabled: bool) -> Self {
         assert!(voltage <= 0b111_1111); // Only 7-bit wide
 
         Self {
             kind: LdoKind::LDO4,
             voltage,
+            enabled,
+        }
+    }
+
+    /// Returns LDO voltage in milliVolts.
+    pub fn voltage(&self) -> MilliVolts {
+        match self.kind {
+            LdoKind::LDO2 => {
+                assert!(self.voltage <= 0b1111);
+                MilliVolts(1800 + self.voltage as usize * 100)
+            },
+            LdoKind::LDO3 => {
+                assert!(self.voltage <= 0b1111);
+                MilliVolts(1800 + self.voltage as usize * 100)
+            },
+            LdoKind::LDO4 => {
+                assert!(self.voltage <= 0b111_1111);
+                MilliVolts(700 + self.voltage as usize * 25)
+            },
+        }
+    }
+
+    /// Returns `true` if this LDO is currently enabled.
+    pub fn enabled(&self) -> bool {
+        self.enabled
+    }
+
+    pub(crate) fn from_voltage_and_enabled(kind: LdoKind, voltage_bits: u8, ldo_enabled: bool) -> Self {
+        match kind {
+            LdoKind::LDO2 => Ldo::ldo2_with_voltage(voltage_bits, ldo_enabled),
+            LdoKind::LDO3 => Ldo::ldo3_with_voltage(voltage_bits, ldo_enabled),
+            LdoKind::LDO4 => Ldo::ldo4_with_voltage(voltage_bits, ldo_enabled),
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -167,6 +167,32 @@ where
         Ok(())
     }
 
+    /// Reads currently active settings of specific LDO from the chip.
+    pub fn read_ldo(&mut self, kind: LdoKind) -> Axp173Result<Ldo, E> {
+        let reg = match kind {
+            LdoKind::LDO2 | LdoKind::LDO3 => LDO2_LDO3_OUT_VOL_REG,
+            LdoKind::LDO4 => LDO4_OUT_VOL_REG,
+        };
+
+        let voltage_bits = self.read_u8(reg).map_err(Error::I2c)?;
+        let bits_range = match kind {
+            LdoKind::LDO2 => 4..8,
+            LdoKind::LDO3 => 0..4,
+            LdoKind::LDO4 => 0..7,
+        };
+        let voltage_bits = voltage_bits.get_bits(bits_range);
+
+        let enabled_bit = match kind {
+            LdoKind::LDO2 => POWER_ON_OFF_REG_LDO2_ON,
+            LdoKind::LDO3 => POWER_ON_OFF_REG_LDO3_ON,
+            LdoKind::LDO4 => POWER_ON_OFF_REG_LDO4_ON,
+        };
+        let enabled_bits = self.read_u8(POWER_ON_OFF_REG).map_err(Error::I2c)?;
+        let ldo_enabled = enabled_bits.get_bit(enabled_bit);
+
+        Ok(Ldo::from_voltage_and_enabled(kind, voltage_bits, ldo_enabled))
+    }
+
     /// Sets charging current of the battery. Adjust this for an efficient and safe charging of
     /// your lithium battery's capacity.
     pub fn set_charging_current(&mut self, current: ChargingCurrent) -> OperationResult<E> {
